4a9d2c364e41969c0d63a08492a5699ea1e6656f
Joshua Rucker <yourreason225@gmail.com>
2025-12-17 07:38:53 -0600

    Update Lesson 17 todo List

--- Let Phil Lessons/Lesson17-js/script.js
+++ Let Phil Lessons/Lesson17-js/script.js
@@ -1,9 +1,19 @@
-//script.js
-
-console.log("JS FILE LOADED");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
 function addTask() {
-    console.log("Hey");
+    const taskText = taskInput.value.trim();
+    if(taskText === "") return;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskText;
+    taskItem.classList.add("task")
+
+    // add a click event to remove task
+    taskItem.addEventListener("click", function() {
+        taskList.removeChild(taskItem);
+    });
+    taskList.appendChild(taskItem);
+
+    taskInput.value = "";
 }
